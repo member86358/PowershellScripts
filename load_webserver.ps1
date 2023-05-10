bc7a1be9080da922be5122388ebff4e877d0f858
David <50872980+member86358@users.noreply.github.com>
2023-05-10 10:15:23 +0200

    Add files via upload

---
+++ load_webserver.ps1
@@ -0,0 +1,8 @@
+﻿$wshell = New-Object -ComObject WScript.Shell  
+For ($i=0; $i -le 100; $i++) {
+    Start-Sleep -Milliseconds 20
+    Write-Progress -Activity "Loading..." -Status "Brave Browser..." -PercentComplete $i
+}
+Start-Process -FilePath '"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"'  http://192.168.77.120/awp//intro.html
+Start-Sleep -Seconds 2
+$wshell.SendKeys('{F11}')
